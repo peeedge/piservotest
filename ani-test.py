17b03ff2b79639197f5f3c20ba6889131387ae0b
Kajgohan <hogan8b1@gmail.com>
2017-06-07 13:10:52 -0400

    Update ani-test.py

--- ani-test.py
+++ ani-test.py
@@ -12,23 +12,27 @@ Channel = []
 Gain = []
 Resolution = []
 Settling = []
-print(len(sys.argv))
+#print(len(sys.argv))
 
 for r in xrange(2, len(sys.argv)):
-	print(r)
+	#print(r)
 	Channel.append(int(sys.argv[r]))
 	Gain.append(0)
 	Resolution.append(12)
 	Settling.append(0)
 
 feedbackArguments = []
+print('Time, PT1-Pin {0}, PT2-pin {1}, PT3-Pin {2}, PT4-Pin {3}'.format(sys.argv[2], sys.argv[3], sys.argv[4], sys.argv[5]))
 for i in xrange(len(sys.argv) - 2):
    feedbackArguments.append(u6.AIN24(PositiveChannel=Channel[i], ResolutionIndex=Resolution[i], GainIndex=Gain[i], SettlingFactor=Settling[i]))
 #print feedbackArguments
 for x in xrange(1, dataPoints):
 	time.sleep(.01)
 	ainBits = d.getFeedback(feedbackArguments)
-	for e in xrange(len(sys.argv)-2):
-		print(sys.argv[e+2], end=': ')
-		print(d.binaryToCalibratedAnalogVoltage(0, ainBits[e], is16Bits = False, resolutionIndex = 0)) 
+	print(time.clock(), end=',')
+	for e in xrange(0, len(sys.argv)-2, 4):
+		print(d.binaryToCalibratedAnalogVoltage(0, ainBits[e], is16Bits = False, resolutionIndex = 0), end=',')
+		print(d.binaryToCalibratedAnalogVoltage(0, ainBits[e+1], is16Bits = False, resolutionIndex = 0), end=',')
+		print(d.binaryToCalibratedAnalogVoltage(0, ainBits[e+2], is16Bits = False, resolutionIndex = 0), end=',')
+		print(d.binaryToCalibratedAnalogVoltage(0, ainBits[e+3], is16Bits = False, resolutionIndex = 0)) 
 #print(ainBits)
