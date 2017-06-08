c1c1b4792f05954db3840db6dad0337c8f4bc755
Kajgohan <hogan8b1@gmail.com>
2017-06-08 13:47:30 -0400

    Create digiOutMux.py

--- digiOutMux.py
+++ digiOutMux.py
@@ -11,22 +11,9 @@ def convert(inByte2, inByte3):
     combined = inByte2*256 + inByte3
     return combined
 
-#on15 = (u6.BitDirWrite(16, 1), u6.BitStateWrite(16, 1))
-##off15 = (u6.BitDirWrite(16, 1), u6.BitStateWrite(16, 0))
-#on16 = (u6.BitDirWrite(17, 1), u6.BitStateWrite(17, 1))
-#off16 = (u6.BitDirWrite(17, 1), u6.BitStateWrite(17, 0))
-#on17 = (u6.BitDirWrite(18, 1), u6.BitStateWrite(18, 1))
-#off17 = (u6.BitDirWrite(18, 1), u6.BitStateWrite(18, 0))
-
 
  for q in xrange(int(sys.argv[1])):
  	cmdList=[]
- 	#time.sleep(1)
- #	state15 = (q%8)//4
- ##	if state15 >= 1:
- #		s15 = 1
- #	else:
- #		s15 = 0
 
  	state16 = (q%4)//2
  	if state16 >= 1:
@@ -38,11 +25,10 @@ def convert(inByte2, inByte3):
 
  	mux16 = (u6.BitDirWrite(16, 1), u6.BitStateWrite(16, s15))
  	mux17 = (u6.BitDirWrite(17, 1), u6.BitStateWrite(17, s16))
- #	mux18 = (u6.BitDirWrite(18, 1), u6.BitStateWrite(18, state17))
+ 
 
  	cmdList.append(mux16)
  	cmdList.append(mux17)
- #	cmdList.append(mux18)
  	d.getFeedback(cmdList)
  	
 
@@ -55,9 +41,6 @@ def convert(inByte2, inByte3):
     results = d.spi([0x00, 0])
     inByte3 = results['SPIBytes'][0]
 
-    # print(inByte1, end=', ')
-    # print(inByte2, end=', ')
-    # print(inByte3)
     print(s15, s16, end=':')
 
     print (convert(inByte2, inByte3))
